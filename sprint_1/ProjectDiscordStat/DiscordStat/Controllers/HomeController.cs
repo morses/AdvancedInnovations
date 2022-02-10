35d691fefdc7006646d53eceefa143c91fae6407
Korbin Cardoza <kcardoza17@wou.edu>
2022-02-10 11:43:00 -0800

    Finished Getting User information from Discord API

--- sprint_1/ProjectDiscordStat/DiscordStat/Controllers/HomeController.cs
+++ sprint_1/ProjectDiscordStat/DiscordStat/Controllers/HomeController.cs
@@ -1,6 +1,16 @@
 ﻿using DiscordStats.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using RestSharp;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Azure.Core;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace DiscordStats.Controllers
 {
@@ -17,9 +27,10 @@ namespace DiscordStats.Controllers
         {
             return View();
         }
-
+        [Authorize(AuthenticationSchemes = "Discord")]
         public IActionResult Privacy()
         {
+
             return View();
         }
 
@@ -28,5 +39,47 @@ namespace DiscordStats.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult DiscordAuthFailed()
+        {
+            return View();
+        }
+        
+
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public  IActionResult Account()
+        {
+
+
+            ViewBag.id = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            ViewBag.name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+
+            var request = (HttpWebRequest)WebRequest.Create("https://discord.com/api/users/@me/guilds");
+            request.Method = "GET";
+            var t = User;
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer " + User.Claims.First(c => c.Type == ClaimTypes.Role).Value);
+            request.Headers.Add("Content-Type", "application/json");
+
+            var content = string.Empty;
+           
+            
+
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+            var server = JsonConvert.DeserializeObject<List<Server>>(content);
+
+            return View(server);
+        }
+
     }
 }
