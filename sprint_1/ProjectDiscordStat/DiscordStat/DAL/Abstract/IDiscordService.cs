70f13cf5bf46d28f9fd0b0f3cc94918d45a9b2f9
Scot Morse <morses@wou.edu>
2022-02-11 08:47:14 -0800

    Step 2 of refactoring the use of the Discord API -- split it at the seams and then use parameter injection to create the ability to inject a fake

--- sprint_1/ProjectDiscordStat/DiscordStat/DAL/Abstract/IDiscordService.cs
+++ sprint_1/ProjectDiscordStat/DiscordStat/DAL/Abstract/IDiscordService.cs
@@ -2,8 +2,20 @@ using DiscordStats.Models;
 
 namespace DiscordStats.DAL.Abstract
 {
+    // This is how we will allow mocking of GetJsonStringFromEndpoint.  It is a delegate method
+    // See: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates/
+    // It is like a function pointer in C++, or how you can "pass a function" to a method like
+    // we've been doing in LINQ expressions all along
+    public delegate string SendRequest(string bearerToken, string uri);
+
     public interface IDiscordService
     {
-        public IEnumerable<Server>? GetCurrentUserGuilds(string bearerToken);
+        /// <summary>
+        /// Get all servers for the user with the given token and the default message sender.
+        /// </summary>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
+        List<Server>? GetCurrentUserGuilds(string bearerToken);
+
     }
 }
--- sprint_1/ProjectDiscordStat/DiscordStat/DAL/Concrete/DiscordService.cs
+++ sprint_1/ProjectDiscordStat/DiscordStat/DAL/Concrete/DiscordService.cs
@@ -1,21 +1,22 @@
 using DiscordStats.DAL.Abstract;
 using DiscordStats.Models;
-
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Azure.Core;
 using System.Net;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace DiscordStats.DAL.Concrete
 {
+    // It would be nice to use constructor injection here to inject the ability to get a web request
+    // but that won't work in cases like this where we'd have to know the User and their bearer token
+    // before creating the service to inject.  Instead we use method parameter injection.
+
     public class DiscordService : IDiscordService
     {
-        public IEnumerable<Server>? GetCurrentUserGuilds(string bearerToken)
+        // This method wraps up the minimum functionality needed to make a request
+        // to an external dependency.  It cannot be unit tested, but we do need to fake it in
+        // order to test code that uses it, i.e. GetCurrentUserGuilds below
+        public static string GetJsonStringFromEndpoint(string bearerToken, string uri)
         {
-            var request = (HttpWebRequest)WebRequest.Create("https://discord.com/api/users/@me/guilds");
+            var request = (HttpWebRequest)WebRequest.Create(uri);
             request.Method = "GET";
             request.ContentType = "application/json";
             request.Headers.Add("Authorization", "Bearer " + bearerToken);
@@ -33,7 +34,24 @@ namespace DiscordStats.DAL.Concrete
                     }
                 }
             }
-            IEnumerable<Server>? servers = JsonConvert.DeserializeObject<List<Server>>(content);
+            return content;
+        }
+
+        // This is the method we can use from the controller, since the controller (usually) doesn't need to know
+        // anything about how this information is acquired
+        public List<Server>? GetCurrentUserGuilds(string bearerToken)
+        {
+            SendRequest sr = GetJsonStringFromEndpoint;
+            return GetCurrentUserGuilds( bearerToken, sr);
+        }
+
+        // But this is the method that actually does the work AND can have fake data injected via the delegate method
+        public List<Server>? GetCurrentUserGuilds(string bearerToken, SendRequest messageSender)
+        {
+            // Remember to handle errors here
+            string response = messageSender(bearerToken, "https://discord.com/api/users/@me/guilds");
+            // And here
+            List<Server>? servers = JsonConvert.DeserializeObject<List<Server>>(response);
             return servers;
         }
     }
