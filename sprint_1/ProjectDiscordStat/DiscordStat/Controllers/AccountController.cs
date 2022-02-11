d1ff05b7bffe8a0e242ce7b38a04f48ceff6e454
Korbin Cardoza <kcardoza17@wou.edu>
2022-02-11 12:26:55 -0800

    Added page to add bot to servers that user owns

---
+++ sprint_1/ProjectDiscordStat/DiscordStat/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Security.Claims;
+using DiscordStats.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DiscordStats.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public IActionResult Account()
+        {
+            ViewBag.id = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            ViewBag.name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+            var request = (HttpWebRequest)WebRequest.Create("https://discord.com/api/users/@me/guilds");
+            request.Method = "GET";
+            var t = User;
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer " + User.Claims.First(c => c.Type == ClaimTypes.Role).Value);
+            request.Headers.Add("Content-Type", "application/json");
+
+            var content = string.Empty;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+            var server = JsonConvert.DeserializeObject<List<Server>>(content);
+
+            return View(server);
+        }
+
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public IActionResult Servers()
+        {
+
+
+            ViewBag.id = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            ViewBag.name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+
+            var request = (HttpWebRequest)WebRequest.Create("https://discord.com/api/users/@me/guilds");
+            request.Method = "GET";
+            var t = User;
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer " + User.Claims.First(c => c.Type == ClaimTypes.Role).Value);
+            request.Headers.Add("Content-Type", "application/json");
+
+            var content = string.Empty;
+
+
+
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+            var server = JsonConvert.DeserializeObject<List<Server>>(content);
+           
+
+            return View(server.Where(m => m.Owner == "true").ToList());
+        }
+    }
+
+}
--- sprint_1/ProjectDiscordStat/DiscordStat/Controllers/HomeController.cs
+++ sprint_1/ProjectDiscordStat/DiscordStat/Controllers/HomeController.cs
@@ -44,42 +44,6 @@ namespace DiscordStats.Controllers
         {
             return View();
         }
-        
-
-        [Authorize(AuthenticationSchemes = "Discord")]
-        public  IActionResult Account()
-        {
-
-
-            ViewBag.id = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            ViewBag.name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
-
-            var request = (HttpWebRequest)WebRequest.Create("https://discord.com/api/users/@me/guilds");
-            request.Method = "GET";
-            var t = User;
-            request.ContentType = "application/json";
-            request.Headers.Add("Authorization", "Bearer " + User.Claims.First(c => c.Type == ClaimTypes.Role).Value);
-            request.Headers.Add("Content-Type", "application/json");
-
-            var content = string.Empty;
-           
-            
-
-
-            using (var response = (HttpWebResponse)request.GetResponse())
-            {
-                using (var stream = response.GetResponseStream())
-                {
-                    using (var sr = new StreamReader(stream))
-                    {
-                        content = sr.ReadToEnd();
-                    }
-                }
-            }
-            var server = JsonConvert.DeserializeObject<List<Server>>(content);
-
-            return View(server);
-        }
-
+ 
     }
 }
