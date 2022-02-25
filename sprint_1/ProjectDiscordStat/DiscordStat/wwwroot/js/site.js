90596c8e012af08d30179cb26c211495cc0cae55
Abraham Vela <abvela09@mail.wou.edu>
2022-02-24 20:04:43 -0800

    The account page is simple to follow and I made sort of a page theme.

--- sprint_1/ProjectDiscordStat/DiscordStat/wwwroot/js/site.js
+++ sprint_1/ProjectDiscordStat/DiscordStat/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//$(".change").on("click", function () {
+//    if ($("body").hasClass("dark")) {
+//        $("body").removeClass("dark");
+
+//    } else {
+//        $("body").addClass("dark");
+
+//    }
+//});
+
+/*setInterval(function () { $("body").addClass("dark") });*/
